5ffd94b8a77386cf0ea4dc4ba2124870c713dd84
TLohinava <lohinava.t@gmail.com>
2022-09-27 15:17:10 +0300

    Initial commit

---
+++ src/main/java/com/solvd/atm/MainClass.java
@@ -0,0 +1,13 @@
+package com.solvd.atm;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainClass {
+
+    private static final Logger LOGGER = LogManager.getLogger(MainClass.class);
+
+    public static void main(String[] args) {
+
+    }
+}
