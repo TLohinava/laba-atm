db8f6f45f9256b4d86597fd4ec427840d2172ffa
TLohinava <lohinava.t@gmail.com>
2022-09-29 12:13:38 +0300

    Check for zero in scanner was added

--- src/main/java/com/solvd/atm/Utils.java
+++ src/main/java/com/solvd/atm/Utils.java
@@ -7,12 +7,16 @@ public class Utils {
 
     public static BigDecimal enterSum() {
         Scanner scanner = new Scanner(System.in);
-        BigDecimal sum = null;
+        BigDecimal sum;
         System.out.println("Please enter the required sum: ");
         if (scanner.hasNextBigDecimal()) {
             sum = scanner.nextBigDecimal();
+            if(sum.compareTo(BigDecimal.ZERO) == 0) {
+                System.out.println("Sorry, the sum cannot be 0. ");
+                sum = enterSum();
+            }
         } else {
-            System.out.println("Sorry, your data is invalid. Please enter the digits.");
+            System.out.println("Sorry, the sum should be numeric. ");
             sum = enterSum();
         }
         return sum;
