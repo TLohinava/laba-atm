d1ea04574cd550083f124593781fa90d7dfb81b4
MaryiaTsvilik <maryia.tsvilik@gmail.com>
2022-10-08 19:39:14 +0300

    CurrencyType has been updated

--- src/main/java/com/solvd/atm/Utils.java
+++ src/main/java/com/solvd/atm/Utils.java
@@ -3,7 +3,6 @@ package com.solvd.atm;
 import com.solvd.atm.domain.Atm;
 import com.solvd.atm.domain.Card;
 import com.solvd.atm.domain.CurrencyType;
-import com.solvd.atm.domain.IUseATM;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
@@ -30,19 +29,86 @@ public class Utils {
     public static void withdrawCash(Atm atm, Card card) {
         BigDecimal sum = enterSum();
         boolean checkAtm = atm.checkBalance(sum, CurrencyType.BYN);
-        //change currencyType in atm method for week two
         boolean checkCard = card.checkBalance(sum, card.getCurrencyType());
         if (checkAtm && checkCard) {
             card.withdraw(sum);
             atm.withdraw(sum);
         }
+    }
+
+    public static  currencySelection(Atm atm, CurrencyType currencyType){
+        Scanner scanner = new Scanner(System.in);
 
     }
 
-    public static void useATM(IUseATM useATM, Card card) {
-        useATM.getMenu(card);
+    public static String getResult(BigDecimal inCount, int from, int to) {
+        BigDecimal result = BigDecimal.valueOf(0);
+        switch (from) {
+            case 0:
+                switch (to) {
+                    case 0:
+                        break;
+                    case 1:
+                        result = inCount / getUSDrate();
+                        break;
+                    case 2:
+                        result = inCount / getEURrate();
+                        ;
+                        break;
+                    case 3:
+                        result = inCount / getUAHrate() * 10;
+                        break;
+                }
+                break;
+            case 1:
+                switch (to) {
+                    case 0:
+                        result = inCount * getUSDrate();
+                        break;
+                    case 1:
 
+                        break;
+                    case 2:
+                        result = inCount * getUSDrate() / getEURrate();
+                        break;
+                    case 3:
+                        result = inCount * getUSDrate() / getUAHrate() * 10;
+                        break;
+                }
+                break;
+            case 2:
+                switch (to) {
+                    case 0:
+                        result = inCount * getEURrate();
+                        break;
+                    case 1:
+                        result = inCount * getEURrate() / getUSDrate();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        result = inCount * getEURrate() / getUAHrate() * 10;
+                        break;
+                }
+                break;
+            case 3:
+                switch (to) {
+                    case 0:
+                        result = inCount * getUAHrate() / 10;
+                        break;
+                    case 1:
+                        result = inCount * getUAHrate() / 10 / getUSDrate();
+                        break;
+                    case 2:
+                        result = inCount * (getUAHrate() / 10) / getEURrate();
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+        }
+        String s = String.format("%.4f", result);
+        return s;
     }
-
-
 }
--- src/main/java/com/solvd/atm/domain/CurrencyType.java
+++ src/main/java/com/solvd/atm/domain/CurrencyType.java
@@ -2,6 +2,23 @@ package com.solvd.atm.domain;
 
 public enum CurrencyType {
 
-    BYN, RUB, EUR, USD, CNY
+    BYN("BYN"),
+    RUB("RUB"),
+    EUR("EUR"),
+    USD("USD"),
+    CNY("CNY");
 
+    private String type;
+
+    CurrencyType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
