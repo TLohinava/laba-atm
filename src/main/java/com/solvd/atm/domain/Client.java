e4b107abb10344b05ed0fea1f4dde018133a8993
Diana Melnikova <melnikova.diana111@gmail.com>
2022-10-09 17:00:48 +0300

    Unused imports were deleted in Client class

--- src/main/java/com/solvd/atm/domain/Client.java
+++ src/main/java/com/solvd/atm/domain/Client.java
@@ -1,8 +1,6 @@
 package com.solvd.atm.domain;
 
 import com.solvd.atm.Utils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Client implements IUseATM {
 
